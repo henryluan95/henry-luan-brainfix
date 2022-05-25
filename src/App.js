30839e89009e12e0a0d847281c6689c4943a3b34
Henry Luan <henryluan95@gmail.com>
2022-05-25 09:33:26 -0400

    Forgot to switch branch lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.scss";
-
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Hero />
+      </main>
       <section></section>
     </>
   );
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Hero.scss";
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <div className="hero">
+        <video
+          className="hero__video"
+          poster="https://i.imgur.com/l2Xfgpl.jpg"
+          src="https://project-2-api.herokuapp.com/stream"
+        ></video>
+      </div>
+    );
+  }
+}
+
+export default Hero;
