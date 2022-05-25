47652c3d8f85fc7618ca1855fedceba84f0e031a
Henry Luan <henryluan95@gmail.com>
2022-05-24 21:05:17 -0400

    Created a navbar html structure

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./Navbar.scss";
+import image from "../../assets/Images/Mohan-muruge.jpg";
 
 class Navbar extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <nav>
+        <div className="nav">
+          <a href="/" className="nav__logo"></a>
+          <div className="nav__items">
+            <label>
+              <input className="nav__search" type="text" placeholder="search" />
+            </label>
+            <button> UPLOAD</button>
+            <img
+              src={image}
+              className="nav__profile-picture"
+              alt="profile avatar"
+            />
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
