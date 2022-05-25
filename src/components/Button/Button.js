29c72051585c1e27154c7016c2884df7612f3202
Henry Luan <henryluan95@gmail.com>
2022-05-24 22:16:22 -0400

    Refactored Button Component to accept class prop, Created scss for button component and Created scss for mobile navbar

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ import uploadIcon from "../../assets/Icons/upload.svg";
 class Button extends React.Component {
   render() {
     return (
-      <button className="button">
+      <button className={`button ${this.props.class}`}>
         <img className="button__icon" src={uploadIcon} alt="upload Icon" />
         {this.props.text.toUpperCase()}
       </button>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Navbar.scss";
-import image from "../../assets/Images/Mohan-muruge.jpg";
 import Button from "../Button/Button";
+import profileImage from "../../assets/Images/Mohan-muruge.jpg";
+import brainFixLogo from "../../assets/Logo/BrainFlix-logo.svg";
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav>
         <div className="nav">
-          <a href="/" className="nav__logo"></a>
+          <a href="/" className="nav__logo">
+            <img className="nav__logo-image" src={brainFixLogo} alt="" />
+          </a>
           <div className="nav__items">
             <label>
-              <input className="nav__search" type="text" placeholder="search" />
+              <input className="nav__search" type="text" placeholder="Search" />
             </label>
-            <Button text="Upload" />
+            <Button text="Upload" class="nav__button" />
             <img
-              src={image}
+              src={profileImage}
               className="nav__profile-picture"
               alt="profile avatar"
             />
