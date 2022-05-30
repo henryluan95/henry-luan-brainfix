4eed30c052a94aec32656bacf4d6c6583694d1dc
Henry Luan <henryluan95@gmail.com>
2022-05-30 09:47:16 -0400

    Create a Homepage and Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import "./App.scss";
 import { Component } from "react";
-import detailedVideoList from "./data/video-details.json";
-import videoList from "./data/videos.json";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Info from "./components/Info/Info";
-import Recommendation from "./components/Recommendation/Recommendation";
+import Home from "./pages/Home/Home";
 
 class App extends Component {
-  state = {
-    selectedMovie: detailedVideoList[0],
-    detailedVideoList,
-  };
-
-  changeVideo = (videoId) => {
-    this.setState({
-      selectedMovie: detailedVideoList.find((video) => video.id === videoId),
-    });
-
-    window.scroll({ top: 0, behavior: "smooth" });
-  };
-
-  //Create method to filter in all unselected videos
-  UnselectedMovieList = () => {
-    return videoList.filter(
-      (video) => video.id !== this.state.selectedMovie.id
-    );
-  };
-
   render() {
     return (
       <>
         <header>
           <Navbar />
         </header>
-        <main>
-          <Hero selectedMovie={this.state.selectedMovie} />
-          <div className="main-content">
-            <Info selectedMovie={this.state.selectedMovie} />
-            <Recommendation
-              movieList={this.UnselectedMovieList()}
-              changeVideo={this.changeVideo}
-            />
-          </div>
-        </main>
+        <Home />
       </>
     );
   }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,48 @@
+import "./Home.scss";
+import { Component } from "react";
+import detailedVideoList from "../../data/video-details.json";
+import videoList from "../../data/videos.json";
+import Hero from "../../components/Hero/Hero";
+import Info from "../../components/Info/Info";
+import Recommendation from "../../components/Recommendation/Recommendation";
+
+class Home extends Component {
+  state = {
+    selectedMovie: detailedVideoList[0],
+    detailedVideoList,
+  };
+
+  changeVideo = (videoId) => {
+    this.setState({
+      selectedMovie: detailedVideoList.find((video) => video.id === videoId),
+    });
+
+    window.scroll({ top: 0, behavior: "smooth" });
+  };
+
+  //Create method to filter in all unselected videos
+  UnselectedMovieList = () => {
+    return videoList.filter(
+      (video) => video.id !== this.state.selectedMovie.id
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <main>
+          <Hero selectedMovie={this.state.selectedMovie} />
+          <div className="main-content">
+            <Info selectedMovie={this.state.selectedMovie} />
+            <Recommendation
+              movieList={this.UnselectedMovieList()}
+              changeVideo={this.changeVideo}
+            />
+          </div>
+        </main>
+      </>
+    );
+  }
+}
+
+export default Home;
